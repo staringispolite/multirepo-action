5723ec228b41c50cefc3304271272bceba2b1872
Ronan McCarter <63772591+rpmccarter@users.noreply.github.com>
2024-01-11 01:13:51 -0800

    add git functionality

--- dist/index.js
+++ dist/index.js
@@ -1,39 +1,56 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import { writeFile, readFile } from 'fs/promises';
+import { parse } from 'yaml';
+const execOrThrow = async (...args) => {
+    const exitCode = await exec.exec(...args);
+    if (exitCode !== 0)
+        throw Error(`error running command: ${args[0]} ${args[1]?.join(' ') ?? ''}`);
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-const core = __importStar(require("@actions/core"));
-const fs_1 = require("fs");
+const setToken = async (token) => {
+    const encodedToken = Buffer.from(`x-access-token:${token}`, 'utf-8').toString('base64');
+    core.setSecret(encodedToken);
+    const headerPlaceholder = 'Authorization: basic ***';
+    const headerValue = `Authorization: basic ${encodedToken}`;
+    const headerKey = 'http.https://github.com/.extraheader';
+    const configPath = '.git/config';
+    await execOrThrow('git', ['config', '--local', headerKey, headerPlaceholder]);
+    const configString = await readFile(configPath, 'utf-8');
+    await writeFile(configPath, configString.replace(headerPlaceholder, headerValue));
+    return () => execOrThrow('git', ['config', '--local', '--unset-all', headerKey]);
+};
+let resetToken;
 try {
-    const subdirectory = core.getInput('subdirectory');
+    const token = core.getInput('token');
+    const repos = parse(core.getInput('repos'));
     const targetBranch = core.getInput('target-branch');
-    const dir = (0, fs_1.readdirSync)(subdirectory);
-    console.log(dir);
+    const subdirectory = core.getInput('subdirectory');
+    await execOrThrow('cd', [subdirectory]);
+    resetToken = await setToken(token);
+    for (const { owner, repo, ref } of repos) {
+        await execOrThrow('rm', ['-rf', repo]);
+        const args = ['clone', '--depth=1'];
+        if (ref)
+            args.push(`--branch="${ref}"`);
+        args.push(`https://github.com/${owner}/${repo}`);
+        await execOrThrow('git', args);
+    }
+    await execOrThrow('git', ['add', '.']);
+    const hasChanges = await exec.exec('git', ['diff-index', '--quiet', '--cached', 'HEAD', '--']) !== 0;
+    if (!hasChanges) {
+        console.log('No changes detected, skipping...');
+    }
+    else {
+        await execOrThrow('git', ['commit', '-m', 'update']);
+        await execOrThrow('git', ['push', 'origin', `HEAD:${targetBranch}`]);
+    }
 }
 catch (error) {
     const message = error && typeof error === 'object' && 'message' in error && typeof error.message === 'string'
         ? error.message
         : JSON.stringify(error, null, 2);
     core.setFailed(message);
+}
+finally {
+    resetToken?.();
 }
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,66 @@
 import * as core from '@actions/core';
-import * as github from '@actions/github';
-import { readdirSync } from 'fs';
-import type { MintConfig } from '@mintlify/models';
+import * as exec from '@actions/exec';
+import { writeFile, readFile } from 'fs/promises';
+import { parse } from 'yaml';
 
+type Repo = {
+  owner: string;
+  repo: string;
+  ref?: string;
+}
+
+const execOrThrow: (...args: Parameters<typeof exec.exec>) => Promise<void> = async (...args) => {
+  const exitCode = await exec.exec(...args);
+  if (exitCode !== 0) throw Error(`error running command: ${args[0]} ${args[1]?.join(' ') ?? ''}`);
+}
+
+const setToken = async (token: string) => {
+  const encodedToken = Buffer.from(`x-access-token:${token}`, 'utf-8').toString('base64');
+  core.setSecret(encodedToken);
+  const headerPlaceholder = 'Authorization: basic ***';
+  const headerValue = `Authorization: basic ${encodedToken}`;
+  const headerKey = 'http.https://github.com/.extraheader';
+  const configPath = '.git/config';
+
+  await execOrThrow('git', ['config', '--local', headerKey, headerPlaceholder]);
+  const configString = await readFile(configPath, 'utf-8');
+  await writeFile(configPath, configString.replace(headerPlaceholder, headerValue));
+
+  return () => execOrThrow('git', ['config', '--local', '--unset-all', headerKey]);
+}
+
+let resetToken;
 try {
-  const subdirectory = core.getInput('subdirectory');
+  const token = core.getInput('token');
+  const repos = parse(core.getInput('repos')) as Repo[];
   const targetBranch = core.getInput('target-branch');
+  const subdirectory = core.getInput('subdirectory');
 
-  const dir = readdirSync(subdirectory);
-  console.log(dir);
+  await execOrThrow('cd', [subdirectory]);
+  resetToken = await setToken(token);
+  for (const { owner, repo, ref } of repos) {
+    await execOrThrow('rm', ['-rf', repo]);
 
+    const args = ['clone', '--depth=1'];
+    if (ref) args.push(`--branch="${ref}"`);
+    args.push(`https://github.com/${owner}/${repo}`);
+
+    await execOrThrow('git', args);
+  }
+
+  await execOrThrow('git', ['add', '.']);
+  const hasChanges = await exec.exec('git', ['diff-index', '--quiet', '--cached', 'HEAD', '--']) !== 0;
+  if (!hasChanges) {
+    console.log('No changes detected, skipping...');
+  } else {
+    await execOrThrow('git', ['commit', '-m', 'update']);
+    await execOrThrow('git', ['push', 'origin', `HEAD:${targetBranch}`]);
+  }
 } catch (error) {
   const message = error && typeof error === 'object' && 'message' in error && typeof error.message === 'string'
     ? error.message
     : JSON.stringify(error, null, 2);
   core.setFailed(message);
+} finally {
+  resetToken?.();
 }
